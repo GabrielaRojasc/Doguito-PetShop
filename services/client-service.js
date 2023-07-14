ff06de76528996db43a655b3f4658432cbab029d
GabrielaRojasc <gabriela.rojas.chavaco@hotmail.com>
2023-07-14 15:42:51 -0600

    Se crea nuevamente la carpeta de service bien ubicada junto su arhivo de cliente-service.js

---
+++ services/client-service.js
@@ -0,0 +1,15 @@
+const http = new XMLHttpRequest(); //una clase que comunica entre el frontend y el backend
+
+//Abrir http(metodo,url)
+//crud - metodos http (GET,POST,PUT/PATCH,DELETE)
+//Abrir http(open recibe dos parametros, recibe el metodo,y la url)
+http.open("GET", "http://localhost:3000/perfil"); // en lugar de que el navegador se encargue de obtener la informacion, ponemos que sea js se encaargue de esa tarea
+
+http.send(); //se va a encargar de enviar la peticion, desde nuestro navegar o  pryecto esta saliendo hacia el servidor que se encuentra en esta url
+
+http.onload = ( )=>{ //una vez que cargues o termines de recibir una respuesta, vas a de ejecutar una funcion
+
+    const data = http.response
+    console.log(data); //nos regresa la informacion que tenemos en el servidor
+} 
+console.log(http);
